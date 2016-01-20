0b68c7edc9c04d14112d7ac9678629bff197707e
Keith <kthohr@users.noreply.github.com>
2016-01-20 11:06:26 -0500

    Update copyright

--- R/0_genericLP.R
+++ R/0_genericLP.R
@@ -1,3 +1,24 @@
+################################################################################
+##
+##   Copyright (C) 2015 - 2016 Alfred Galichon
+##
+##   This file is part of the R package TraME.
+##
+##   The R package TraME is free software: you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation, either version 2 of the License, or
+##   (at your option) any later version.
+##
+##   The R package TraME is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##
+##   You should have received a copy of the GNU General Public License
+##   along with TraME. If not, see <http://www.gnu.org/licenses/>.
+##
+################################################################################
+
 genericLP <- function(obj, A, modelsense, rhs, sense, Q=NULL, lb=NULL, ub=NULL, start=NULL){
     #
     if(.trame_lp_options$gurobi_exists==TRUE){
