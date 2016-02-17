3f5b07e849328b2cacb674005140fa1ee5e0a523
alfredgalichon <alfred.galichon@gmail.com>
2016-02-17 09:26:13 -0500

    Restored IPFP inversion in NTU-logit case

--- R/market_NTU_logit.R
+++ R/market_NTU_logit.R
@@ -45,18 +45,18 @@ build_market_NTU_logit <- function(n, m, alpha, gamma, sigma=1, neededNorm=NULL)
 
 solveEquilibrium.NTU_logit = ipfp
 
-# margxInv.NTU_logit = function(xs,mkt,Mu0ys,sigma=1)
-# {
-#   if (is.null(xs)) {xs = 1:mkt$transfers$nbX}
-#   if (!is.null(mkt$neededNorm)) {stop('not supported yet')}
-#   themux0s = inversePWA(mkt$n[xs],t ( t(exp(mkt$transfers$gamma[xs,]/sigma - mkt$transfers$alpha[xs,]/sigma) )* Mu0ys ),mkt$transfers$aux_expalpha[xs,]^(1/sigma)) 
-#   return(themux0s)
-# }
+margxInv.NTU_logit = function(xs,mkt,Mu0ys,sigma=1)
+{
+  if (is.null(xs)) {xs = 1:mkt$transfers$nbX}
+  if (!is.null(mkt$neededNorm)) {stop('not supported yet')}
+  themux0s = inversePWA(mkt$n[xs],t ( t(exp(mkt$transfers$gamma[xs,]/sigma - mkt$transfers$alpha[xs,]/sigma) )* Mu0ys ),mkt$transfers$aux_expalpha[xs,]^(1/sigma)) 
+  return(themux0s)
+}
 
-# margyInv.NTU_logit = function(ys,mkt,Mux0s,sigma=1)
-# {
-#   if (is.null(ys)) {ys = 1:mkt$transfers$nbY}
-#   if (!is.null(mkt$neededNorm)) {stop('not supported yet')}
-#   themu0ys = inversePWA(mkt$m[ys],  t(exp( mkt$transfers$alpha[,ys]/sigma -mkt$transfers$gamma[,ys]/sigma) * Mux0s) ,t(mkt$transfers$aux_expgamma[,ys]^(1/sigma)) ) 
-#   return(themu0ys)
-# }
+margyInv.NTU_logit = function(ys,mkt,Mux0s,sigma=1)
+{
+  if (is.null(ys)) {ys = 1:mkt$transfers$nbY}
+  if (!is.null(mkt$neededNorm)) {stop('not supported yet')}
+  themu0ys = inversePWA(mkt$m[ys],  t(exp( mkt$transfers$alpha[,ys]/sigma -mkt$transfers$gamma[,ys]/sigma) * Mux0s) ,t(mkt$transfers$aux_expgamma[,ys]^(1/sigma)) ) 
+  return(themu0ys)
+}
