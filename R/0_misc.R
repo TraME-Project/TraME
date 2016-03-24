dfecd340829c358854f3ef817ea70b7313132112
Keith <kthohr@users.noreply.github.com>
2016-03-24 17:19:21 -0400

    verify_signature

--- R/0_misc.R
+++ R/0_misc.R
@@ -82,7 +82,21 @@ tests_TraME <- function(nbDraws = 1e3)
 
 verify_signature <- function()
 {
-    hash_vals <- tests_TraME()
+    hash_vals <- suppressMessages(tests_TraME())
     #
-    true_hash <- c("fec78b3876dedcdc07c6e35e57d9e846")
+    true_hash <- c("fec78b3876dedcdc07c6e35e57d9e846","2c60b28fcd51e679483ea713bff62022","c65ddb9f306639769b0cba259e8d3903")
+    #
+    if(identical(hash_vals,true_hash)){
+        message('Test results are correct!\n')
+    }else{
+        if(!identical(hash_vals[1],true_hash[1])){
+            message('There is a problem with arum test results.\n')
+        }
+        if(!identical(hash_vals[2],true_hash[2])){
+            message('There is a problem with equilibrium test results.\n')
+        }
+        if(!identical(hash_vals[3],true_hash[3])){
+            message('There is a problem with estimation test results.\n')
+        }
+    }
 }
