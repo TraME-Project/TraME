e5e079a8a49d36ef1e1fed5f5835d2b092620b76
alfredgalichon <alfred.galichon@gmail.com>
2016-04-06 16:32:43 -0400

    fix

--- R/0_equilibrium.R
+++ R/0_equilibrium.R
@@ -30,7 +30,7 @@ ipfp <- function(market, xFirst=T, notifications=TRUE, debugmode=FALSE, tol=1e-1
     # Computes equilibrium in the logit case via IPFP in the all-logit case
 {
     #
-  if (!("mmfs" %in% "market$types")) {stop("ipfp only defined for market whose types contains mmfs.")}
+  if (!("mmfs" %in% market$types)) {stop("ipfp only defined for market whose types contains mmfs.")}
   mmfs = market$mmfs
     noSingles = !is.null(mms$neededNorm)
     if(noSingles){
