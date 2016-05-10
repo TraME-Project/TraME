9cf59bd59cf742f7ab7085190e88d29be4d83401
Keith <kthohr@users.noreply.github.com>
2016-05-10 11:43:48 -0400

    Change zeroin behaviour

--- src/headers/trame_structs.hpp
+++ src/headers/trame_structs.hpp
@@ -4,6 +4,16 @@
  
 // for use in zeroin()
 struct trame_opt_data {
-    arma::mat expUbarX;
-    arma::mat mubarX;
+    arma::mat* expUbarX;
+    arma::mat* mubarX;
+};
+
+class trame_zeroin_data
+{
+    public:
+        //
+        arma::mat expUbarX;
+        arma::mat mubarX;
+        
+    //private:
 };
