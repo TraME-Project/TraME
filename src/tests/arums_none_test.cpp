e5285c26f562e6df4273c52c9768753225dbe302
Keith O'Hara <kthohr@users.noreply.github.com>
2016-05-16 16:36:53 -0400

    Update arums none test file

---
+++ src/tests/arums_none_test.cpp
@@ -0,0 +1,16 @@
+/*
+ * cd ~/Desktop/SCM/GitHub/TraME/src/tests
+ * clang++ -O2 -Wall -std=c++11 -I/opt/local/include -I/Library/gurobi650/mac64/include arums_none_test.cpp -o arums_none.test -L/Library/gurobi650/mac64/lib -lgurobi_c++ -lgurobi65 -framework Accelerate
+ */
+ 
+#include "armadillo"
+
+#include "../headers/trame_structs.hpp"
+
+#include "../headers/arums_empirical.hpp"
+#include "../headers/arums_none.hpp"
+
+int main()
+{
+    
+}
