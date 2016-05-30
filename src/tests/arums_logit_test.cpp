0d783eae7e283fa679f1fa89968ea22c9a3494e7
Keith O'Hara <kthohr@users.noreply.github.com>
2016-05-30 18:06:34 -0400

    cleanup logit test file

--- src/tests/arums_logit_test.cpp
+++ src/tests/arums_logit_test.cpp
@@ -24,6 +24,8 @@
 
 int main()
 {
+    //
+    // inputs:
     arma::mat U(2,3);
     U  << 1.6 << 3.2 << 1.1 << arma::endr
        << 2.9 << 1.0 << 3.1 << arma::endr;
@@ -32,14 +34,21 @@ int main()
     mu << 1.0 << 3.0 << 1.0 << arma::endr
        << 2.0 << 1.0 << 3.0 << arma::endr;
     
+    //
+    // results
+    printf("\n*===================   Start of testLogit   ===================*\n");
+    printf("\n");
+    printf("Inputs: \n");
     arma::cout << "\nU: \n" << U << arma::endl;
     arma::cout << "mu: \n" << mu << arma::endl;
     //
+    // setup logit class objects
     int nbX = U.n_rows;
     int nbY = U.n_cols;
     
     arma::vec n = arma::sum(mu,1);
-    //
+    
+    // logit object
     logit logits;
     logits.nbX = nbX;
     logits.nbY = nbY;
@@ -49,60 +58,57 @@ int main()
     
     logits.U = U;
     logits.mu = mu;
-    //
-    double G_val = logits.G(n);
+    // empirical object:
+    int n_draws = 1000;
+    empirical logit_sim;
     
-    arma::cout << "G -> mu: \n" << logits.mu_sol << arma::endl;
-    arma::cout << "G(U): \n" << G_val << arma::endl;
-    // 
+    logits.simul(logit_sim, n_draws, (int) 1777);
+    
+    logit_sim.U = U;
+    logit_sim.mu = mu;
+    //
+    // first compute optimal assignment (mu)
+    double G_val = logits.G(n);
+    double G_sim_val = logit_sim.G(n);
+    
+    arma::cout << "G(U) and G-sim(U): \n" << G_val << " and " << G_sim_val << arma::endl;
+    
+    arma::cout << "\nG -> mu: \n" << logits.mu_sol << arma::endl;
+    arma::cout << "G-sim -> mu: \n" << logit_sim.mu_sol << arma::endl;
+    //
+    // solution to dual problem U*
     arma::mat U_star;
+    arma::mat U_star_sim;
+    
     double Gstar_val = logits.Gstar(U_star, n);
+    double Gstar_sim_val = logit_sim.Gstar(n,U_star_sim);
+    
+    arma::cout << "G*(mu) and G*-sim(mu): \n" << Gstar_val << " and " << Gstar_sim_val << arma::endl;
     
     arma::cout << "\n\\nabla G*(\\nabla G(U)): \n" << U_star << arma::endl;
-    arma::cout << "G*(mu): \n" << Gstar_val << arma::endl;
+    arma::cout << "\\nabla G-sim*(\\nabla G-sim(U)): \n" << U_star_sim << arma::endl;
     //
-    arma::mat H;
-    logits.D2G(H, n, true);
+    // Gbar
+    arma::mat mu_bar(2,3);
+    mu_bar.fill(2);
     
-    arma::cout << "\nD2G: \n" << H << arma::endl;
+    arma::mat U_bar_temp, mu_bar_temp;
+    
+    double val_Gbar     = logits.Gbar(U_star,mu_bar,n,U_bar_temp,mu_bar_temp);
+    double val_Gbar_sim = logit_sim.Gbar(U_star_sim,mu_bar,n,U_bar_temp,mu_bar_temp);
+    
+    std::cout << "Gbar val: \n" << val_Gbar << std::endl;
+    std::cout << "Gbar-sim val: \n" << val_Gbar_sim << std::endl;
     //
+    // Hessian tests
+    arma::mat H;
     arma::mat Hstar;
+    
+    logits.D2G(H, n, true);
     logits.D2Gstar(Hstar, n, true);
     
+    arma::cout << "\nD2G: \n" << H << arma::endl;
     arma::cout << "D2G*: \n" << Hstar << arma::endl;
-    /*
-     * Gbar test
-     */
-    arma::mat mubar(2,3);
-    mubar.fill(2);
-
-    arma::mat Ubar_temp, mubar_temp;
-
-    double valGbar = logits.Gbar(U_star,mubar,n,Ubar_temp,mubar_temp);
-    
-    std::cout << "Gbar val: \n" << valGbar << std::endl;
-    /*
-     * Simulated logit
-     */
-    int n_draws = 1000;
-    empirical emp_obj;
-    
-    logits.simul(emp_obj, n_draws, (int) 1777);
-    emp_obj.U = U;
-    emp_obj.mu = mu;
-
-    emp_obj.G(n);
-
-    arma::cout << "\nG emp -> mu: \n" << emp_obj.mu_sol << arma::endl;
-    
-    arma::mat Ustar_emp;
-    emp_obj.Gstar(n,Ustar_emp);
-    
-    arma::cout << "\\nabla G*(\\nabla G(U emp)): \n" << Ustar_emp << arma::endl;
-    //
-    //emp_obj.Gbarx(U.row(0).t(),(mubar.row(0).t())/n(0),Ubar_temp,mubar_temp,0);
-    double valGbarEmp = emp_obj.Gbar(Ustar_emp,mubar,n,Ubar_temp,mubar_temp);
-    std::cout << "Gbar emp val: \n" << valGbarEmp << std::endl;
     //
     return 0;
 }
