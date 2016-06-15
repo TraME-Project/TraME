ffc7400a8b6c1e9b1680eb11afedcb6be661b551
Keith O'Hara <kthohr@users.noreply.github.com>
2016-06-14 20:20:58 -0400

    nlopt

---
+++ src/nlopt/generic_nlopt.c
@@ -0,0 +1,45 @@
+/*
+ * Generic Linear/Quadratic Programming solver using Gurobi
+ * Based on Gurobi's 'dense_c.c' example
+ *
+ * Keith O'Hara
+ * 05/08/2016
+ *
+ * clang -O2 -Wall -I/opt/local/include -I/Library/gurobi650/mac64/include generic_lp.c -c -o generic_lp.o
+ * gcc-mp-5 -O2 -Wall -I/opt/local/include -I/Library/gurobi650/mac64/include generic_lp.c -c -o generic_lp.o
+ */
+ 
+#include <string.h>
+
+#include "generic_nlopt.h"
+
+//
+// Dense setup; should be used with Armadillo memptr based passing 
+int generic_nlopt_C(int n_pars, double* io_val, double* lb, double* ub,
+                    double* c_data,)
+{
+    bool success = false;
+    nlopt_opt opt_trame; 
+
+    opt = nlopt_create(NLOPT_LD_MMA, n); /* algorithm and dimensionality */
+
+    if (lb) {
+        nlopt_set_lower_bounds(opt_trame, lb);
+    }
+
+    if (ub) {
+        nlopt_set_upper_bounds(opt_trame, ub);
+    }
+
+    nlopt_set_min_objective(opt_trame, trame_gbar_obj_func, NULL);
+    nlopt_set_xtol_rel(opt_trame, 1e-7);
+
+    double minf;
+    int res_nlopt = nlopt_optimize(opt, io_val, &minf);
+
+    /* Free model */
+    
+    nlopt_destroy(opt_trame);
+
+    return success;
+}
