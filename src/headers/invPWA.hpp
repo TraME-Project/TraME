f773a995c8c19775fe8ad46bd9bd257e7379fa70
Keith O'Hara <kthohr@users.noreply.github.com>
2016-06-29 21:21:06 -0400

    style change

--- src/headers/invPWA.hpp
+++ src/headers/invPWA.hpp
@@ -31,7 +31,7 @@ arma::vec invPWA (arma::vec a, arma::mat B, arma::mat C, double k)
     int x, y_low, y_up, y_mid, y_incl;
     double small_C_sum, lhs, term_1, term_2;
     //
-    for(x = 0; x < nb_X; x++){
+    for (x=0; x < nb_X; x++) {
         Bx = B.row(x).t();  // transpose to ensure that Bx is a column-vector
         Bx_sort_ind = arma::sort_index(Bx);
         b = Bx.elem(Bx_sort_ind);
@@ -43,7 +43,7 @@ arma::vec invPWA (arma::vec a, arma::mat B, arma::mat C, double k)
         y_low = 1;
         y_up  = nb_Y;
         
-        while(y_up > y_low){
+        while (y_up > y_low) {
             y_mid = y_low + (y_up - y_low)/2.0;
             
             b_low.fill(b(y_low-1));
@@ -51,21 +51,18 @@ arma::vec invPWA (arma::vec a, arma::mat B, arma::mat C, double k)
             
             lhs = k * b(y_mid-1) + arma::sum(small_C % arma::min(b_mid,b));
             //
-            if(lhs == a(x)){
+            if (lhs == a(x)) {
                 y_low = y_mid;
                 y_up = y_mid;
-            }
-            else if(lhs > a(x)){
+            } else if (lhs > a(x)) {
                 y_up = y_mid;
-            }
-            else{
+            } else {
                 y_low = y_mid + 1;
             }
             //
-            if((y_low==1) && ( k * b(y_low-1) + arma::sum(small_C % arma::min(b_low,b)) >= a(x) )){
+            if ((y_low==1) && ( k * b(y_low-1) + arma::sum(small_C % arma::min(b_low,b)) >= a(x) )) {
                 vals(x) = a(x) / (k + small_C_sum);
-            }
-            else{
+            } else {
                 y_incl = y_low - 1;
                 
                 temp_vec = (small_C % b);
