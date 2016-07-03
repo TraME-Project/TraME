2528a8ff7ddb9b652afdf50745b3d48a5e87fcdc
Keith O'Hara <kthohr@users.noreply.github.com>
2016-07-03 11:37:04 -0400

    NLopt without constraints

--- src/nlopt/generic_nlopt.hpp
+++ src/nlopt/generic_nlopt.hpp
@@ -13,6 +13,38 @@
 
 bool generic_nlopt(int n_pars, std::vector<double>& io_val, double& opt_val, double* lb, double* ub,
                    double (*opt_objfn)(const std::vector<double> &x_inp, std::vector<double> &grad, void *opt_data),
+                   trame_nlopt_opt_data opt_data)
+
+{
+    bool success = false;
+
+    nlopt::opt opt_trame(nlopt::LD_MMA, n_pars);
+
+    if (lb) {
+        opt_trame.set_lower_bounds(*lb);
+    }
+    if (ub) {
+        opt_trame.set_upper_bounds(*ub);
+    }
+
+    opt_trame.set_min_objective(*opt_objfn, &opt_data);
+    
+    opt_trame.set_xtol_rel(1e-7);
+    opt_trame.set_maxeval(5000);
+
+    double minf;
+    nlopt::result result = opt_trame.optimize(io_val, minf);
+
+    if (result > 0) {
+        opt_val = minf;
+        success = true;
+    }
+
+    return success;
+}
+
+bool generic_nlopt(int n_pars, std::vector<double>& io_val, double& opt_val, double* lb, double* ub,
+                   double (*opt_objfn)(const std::vector<double> &x_inp, std::vector<double> &grad, void *opt_data),
                    double (*opt_constr)(const std::vector<double> &x_inp, std::vector<double> &grad, void *constr_data),
                    trame_nlopt_opt_data opt_data,
                    trame_nlopt_constr_data constr_data)
