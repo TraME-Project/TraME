4ce743a1e7c217e28bdfbeca089e500473dfbf43
Keith O'Hara <kthohr@users.noreply.github.com>
2016-08-03 19:12:43 -0400

    spacing

--- src/lp/generic_lp.h
+++ src/lp/generic_lp.h
@@ -1,8 +1,10 @@
 #ifndef _generic_lp_H
 #define _generic_lp_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "gurobi_c.h"
+
 int generic_LP_C(int rows, int cols, double* obj, double* A, int model_opt_sense, 
                  double* rhs, char* sense, double* Q, double* lb, double* ub,
                  double* objval, double* sol_mat_X, double* sol_mat_RC,
@@ -16,4 +18,5 @@ int generic_LP_C_sparse(int rows, int cols, double* obj, int numnz,
                  double* rhs, char* sense, double* Q, double* lb, double* ub,
                  double* objval, double* sol_mat_X, double* sol_mat_RC,
                  double* dual_mat_PI, double* dual_mat_SLACK);
+
 #endif
