f3c013aa0e0c7b938d22a506ec9dfb0811241da5
Keith O'Hara <kthohr@users.noreply.github.com>
2016-08-26 02:14:02 +0100

    tests

---
+++ src/trame/tests/equilibrium/eap_nash_test.cpp
@@ -0,0 +1,65 @@
+/*
+ * EAP Nash solver test
+ *
+ * Keith O'Hara
+ * 08/01/2016
+ * 
+ * cd ~/Desktop/SCM/GitHub/TraME/src/trame/tests/equilibrium
+ *
+ * g++-mp-5 -O2 -Wall -std=c++11 -I/opt/local/include -I./../../headers -I/usr/local/include eap_nash_test.cpp -c -o eap_nash_test.o
+ * g++-mp-5 -O2 -Wall -o eap_nash.test eap_nash_test.o -L/opt/local/lib -ltrame -framework Accelerate
+ */
+
+#include "trame.hpp"
+
+int main()
+{
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    //
+    // inputs:
+    int nbX = 8;
+    int nbY = 5;
+
+    arma::vec n = arma::ones(nbX,1);
+    arma::vec m = arma::ones(nbY,1);
+
+    arma::mat alpha  = arma::randu(nbX,nbY);
+    arma::mat gamma  = arma::randu(nbX,nbY);
+    arma::mat lambda = 1 + arma::randu(nbX,nbY);
+    arma::mat zeta   = 1 + arma::randu(nbX,nbY);
+
+    arma::mat phi = alpha + gamma;
+    //
+    // results
+    printf("\n*===================   Start of oap_nash Test   ===================*\n");
+    printf("\n");
+    //
+    // TU
+    arma::mat lambda_LTU = lambda/(lambda+zeta);
+    arma::mat phi_LTU = (lambda%alpha + zeta%gamma) / (lambda+zeta);
+
+    trame::dse<trame::none> dse_obj_LTU;
+    dse_obj_LTU.build_LTU(n,m,lambda_LTU,phi_LTU,false);
+    //
+    arma::vec mux0, mu0y, u, v;
+    arma::mat mu_LTU, residuals;
+    trame::eap_nash(dse_obj_LTU, true, NULL, mu_LTU, mux0, mu0y, u, v);
+
+    std::cout << "Solution of TU-none problem using eap_nash:\n" << std::endl;
+    arma::cout << "mu:\n" << mu_LTU << arma::endl;
+    arma::cout << "u:\n" << u << "\n v:\n" << v << arma::endl;
+    //
+    printf("\n*===================    End of oap_nash Test    ===================*\n");
+    printf("\n");
+    //
+    end = std::chrono::system_clock::now();
+        
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    //
+    return 0;
+}
---
+++ src/trame/tests/markets/mfe_test.cpp
@@ -0,0 +1,81 @@
+/*
+ * MMF class test
+ *
+ * Keith O'Hara
+ * 08/01/2016
+ * 
+ * cd ~/Desktop/SCM/GitHub/TraME/src/trame/tests/markets
+ *
+ * g++-mp-5 -O2 -Wall -std=c++11 -I/opt/local/include -I./../../headers -I/usr/local/include mfe_test.cpp -c -o mfe_test.o
+ * g++-mp-5 -O2 -Wall -o mfe.test mfe_test.o -L/opt/local/lib -ltrame -framework Accelerate
+ */
+
+#include "trame.hpp"
+
+int main()
+{
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    //
+    // inputs:
+    int nbX = 18;
+    int nbY = 5;
+    double sigma = 1;
+
+    arma::vec n = arma::ones(nbX,1);
+    arma::vec m = arma::ones(nbY,1);
+
+    arma::mat alpha  = arma::randu(nbX,nbY);
+    arma::mat gamma  = arma::randu(nbX,nbY);
+    arma::mat lambda = 1 + arma::randu(nbX,nbY);
+    arma::mat zeta   = 1 + arma::randu(nbX,nbY);
+
+    arma::mat phi = alpha + gamma;
+    //
+    // results
+    printf("\n*===================   Start of MFE Test   ===================*\n");
+    printf("\n");
+    //
+    // TU
+    trame::mfe mfe_obj_TU;
+    mfe_obj_TU.build_TU(n,m,phi,&sigma,false);
+
+    trame::mfe mfe_obj_NTU;
+    mfe_obj_NTU.build_NTU(n,m,alpha,gamma,&sigma,false);
+
+    arma::mat lambda_LTU = lambda/(lambda+zeta);
+    arma::mat phi_LTU = (lambda%alpha + zeta%gamma) / (lambda+zeta);
+
+    trame::mfe mfe_obj_LTU;
+    mfe_obj_LTU.build_LTU(n,m,lambda_LTU,phi_LTU,&sigma,false);
+    //
+    //
+    arma::vec mux0, mu0y, u, v;
+    arma::mat mu_TU, U, V;
+    trame::ipfp(mfe_obj_TU, true, NULL, NULL, mu_TU, mux0, mu0y, U, V, u, v);
+
+    arma::cout << "Solution of TU-logit problem using ipfp:\n" << mu_TU << arma::endl;
+
+    arma::mat mu_NTU;
+    trame::ipfp(mfe_obj_NTU, true, NULL, NULL, mu_NTU, mux0, mu0y, U, V, u, v);
+
+    arma::cout << "Solution of NTU-logit problem using ipfp:\n" << mu_NTU << arma::endl;
+
+    arma::mat mu_LTU;
+    trame::ipfp(mfe_obj_LTU, true, NULL, NULL, mu_LTU, mux0, mu0y, U, V, u, v);
+
+    arma::cout << "Solution of LTU-logit problem using ipfp:\n" << mu_LTU << arma::endl;
+    //
+    printf("\n*===================    End of MFE Test    ===================*\n");
+    printf("\n");
+    //
+    end = std::chrono::system_clock::now();
+        
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    //
+    return 0;
+}
