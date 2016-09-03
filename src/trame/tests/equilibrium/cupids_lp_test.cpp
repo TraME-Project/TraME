c0d5cc39c287b3067d3729f211d0da688a2ed697
Keith O'Hara <kthohr@users.noreply.github.com>
2016-09-03 14:26:17 -0400

    finish test

--- src/trame/tests/equilibrium/cupids_lp_test.cpp
+++ src/trame/tests/equilibrium/cupids_lp_test.cpp
@@ -21,7 +21,8 @@ int main()
     int nbX = 5;
     int nbY = 3;
 
-    int nbDraws = 1000;
+    int nbDraws_1 = 1000;
+    int nbDraws_2 = 1000;
 
     arma::vec n = arma::ones(nbX,1);
     arma::vec m = arma::ones(nbY,1);
@@ -35,15 +36,15 @@ int main()
     printf("\n*===================   Start of cupids_lp Test   ===================*\n");
     printf("\n");
     //
-    // TU
+    // TU logit
     trame::dse<trame::empirical> dse_obj_TU;
 
     trame::logit logit_1, logit_2;
     logit_1.build(nbX,nbY,1.0,true);
     logit_2.build(nbY,nbX,1.0,true);
 
-    trame::empirical logit_sim_1 = logit_1.simul(&nbDraws,NULL);
-    trame::empirical logit_sim_2 = logit_2.simul(&nbDraws,NULL);
+    trame::empirical logit_sim_1 = logit_1.simul(&nbDraws_1,NULL);
+    trame::empirical logit_sim_2 = logit_2.simul(&nbDraws_2,NULL);
 
     dse_obj_TU.build_TU(n,m,phi,logit_sim_1,logit_sim_2,false);
     //
@@ -54,7 +55,31 @@ int main()
 
     std::cout << "Solution of TU-logitSim problem using LP:\n" << std::endl;
     arma::cout << "mu:\n" << mu_TU << arma::endl;
-    //arma::cout << "u:\n" << u << "\n v:\n" << v << arma::endl;
+    //
+    // TU probit
+    trame::dse<trame::empirical> dse_obj_TU_2;
+
+    double rho = 0.3;
+
+    trame::probit probit_1, probit_2;
+    probit_1.build(nbX,nbY,rho,true);
+    probit_2.build(nbY,nbX,rho,true);
+
+    probit_1.unifCorrelCovMatrices();
+    probit_2.unifCorrelCovMatrices();
+
+    trame::empirical probit_sim_1 = probit_1.simul(&nbDraws_1,NULL);
+    trame::empirical probit_sim_2 = probit_2.simul(&nbDraws_2,NULL);
+
+    dse_obj_TU_2.build_TU(n,m,phi,probit_sim_1,probit_sim_2,false);
+    //
+    arma::mat mu_TU_2;
+
+    trame::cupids_lp(dse_obj_TU_2, true, mu_TU_2, mux0, mu0y, U, V);
+
+    std::cout << "Solution of TU-probitSim problem using LP:\n" << std::endl;
+    arma::cout << "mu:\n" << mu_TU_2 << arma::endl;
+    //arma::cout << "u:\n" << u << "\n v:\n" << v << arma::endl;*/
     //
     printf("\n*===================    End of cupids_lp Test    ===================*\n");
     printf("\n");
