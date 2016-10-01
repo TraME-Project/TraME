578b852d4a95377e53da0479fd5c56ebebfe64df
Keith O'Hara <kthohr@users.noreply.github.com>
2016-10-01 17:05:41 -0400

    module tests

---
+++ src/tests/modules/logit.R
@@ -0,0 +1,37 @@
+library(TraME)
+#library(gurobi)
+
+logit_obj <- new(logit_R)
+
+U = matrix(c(1.6, 3.2, 1.1, 2.9, 1.0, 3.1),nrow=2,byrow=TRUE)
+mu = matrix(c(1, 3, 1, 2, 1, 3), nrow=2, byrow=TRUE)
+
+nbX = dim(U)[1]
+nbY = dim(U)[2]
+n = c(apply(mu,1,sum))
+
+logit_obj$build(nbX,nbY)
+logit_obj$U = U
+
+sim_obj = logit_obj$simul(10000)
+
+logit_obj$G(n)
+
+resG = logit_obj$G(n,U)
+resG
+resGSim = sim_obj$G(n,U)
+resGSim
+
+resGstar = logit_obj$Gstar(n,resG$mu)
+resGstar
+resGstarSim = sim_obj$Gstar(n,resGSim$mu)
+resGstarSim
+
+mubar = matrix(2,2,3)
+
+resGbar = logit_obj$Gbar(U,mubar,n)
+resGbar
+resGbarSim = sim_obj$Gbar(U,mubar,n)
+resGbarSim
+#
+
---
+++ src/tests/modules/rsc.R
@@ -0,0 +1,39 @@
+library(TraME)
+#library(gurobi)
+
+rsc_obj <- new(rsc_R)
+
+U = matrix(c(1.6, 3.2, 1.1, 2.9, 1.0, 3.1),nrow=2,byrow=TRUE)
+mu = matrix(c(1, 3, 1, 2, 1, 3), nrow=2, byrow=TRUE)
+
+nbX = dim(U)[1]
+nbY = dim(U)[2]
+n = c(apply(mu,1,sum))
+
+zeta = matrix(1,nbX,1) %*% matrix(c(0.1, 0.2, 0.3, 0),1,nbY+1)
+
+rsc_obj$build_beta(zeta,2,2)
+rsc_obj$U = U
+
+sim_obj = rsc_obj$simul(10000)
+
+rsc_obj$G(n)
+
+resG = rsc_obj$G(n,U)
+resG
+resGSim = sim_obj$G(n,U)
+resGSim
+
+resGstar = rsc_obj$Gstar(n,resG$mu)
+resGstar
+resGstarSim = sim_obj$Gstar(n,resGSim$mu)
+resGstarSim
+
+mubar = matrix(2,2,3)
+
+resGbar = rsc_obj$Gbar(U,mubar,n)
+resGbar
+resGbarSim = sim_obj$Gbar(U,mubar,n)
+resGbarSim
+#
+
