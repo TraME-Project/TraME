d5756accf59fd078710d5f032b57b52077e031d3
Keith O'Hara <kthohr@users.noreply.github.com>
2017-01-19 12:58:18 -0500

    Add box constraints

--- src/trame/src/optim/generic_optim.cpp
+++ src/trame/src/optim/generic_optim.cpp
@@ -23,13 +23,13 @@
   ################################################################################*/
 
 /*
- * Generic input to TraME optimization routines
+ * Generic input to optimization routines
  *
  * Keith O'Hara
  * 01/11/2017
  *
  * This version:
- * 01/13/2017
+ * 01/17/2017
  */
 
 #include "trame.hpp"
@@ -39,4 +39,40 @@ bool trame::generic_optim(arma::vec& init_out_vals, std::function<double (const 
     bool success = bfgs(init_out_vals,opt_objfn,opt_data);
     //
     return success;
+}
+
+// box constraints
+
+bool trame::generic_optim(arma::vec& init_out_vals, const arma::vec& lower_bounds, const arma::vec& upper_bounds, std::function<double (const arma::vec& vals_inp, arma::vec& grad, void* opt_data)> opt_objfn, void* opt_data)
+{
+    std::function<double (const arma::vec& vals_inp, arma::vec& grad, void* box_data)> box_objfn = [opt_objfn, lower_bounds, upper_bounds] (const arma::vec& vals_inp, arma::vec& grad, void* opt_data) -> double {
+        //
+        arma::vec vals_inv_trans = logit_inv_trans(vals_inp,lower_bounds,upper_bounds);
+        //
+        double ret = opt_objfn(vals_inv_trans,grad,opt_data);
+        arma::mat jacob_correct = jacob_matrix_logit(vals_inp,lower_bounds,upper_bounds);
+
+        grad = jacob_correct.t() * grad; // correct gradient for transformation
+        //
+        return ret;
+    };
+    
+    arma::vec initial_vals = logit_trans(init_out_vals,lower_bounds,upper_bounds);
+    //
+    bool success = bfgs(initial_vals,box_objfn,opt_data);
+    init_out_vals = logit_inv_trans(initial_vals,lower_bounds,upper_bounds);
+    //
+    return success;
+}
+
+arma::mat trame::jacob_matrix_logit(const arma::vec& trans_vals, const arma::vec& lower_bounds, const arma::vec& upper_bounds)
+{
+    int n_vals = trans_vals.n_elem;
+    arma::mat ret_mat = arma::zeros(n_vals,n_vals);
+
+    for (int i=0; i < n_vals; i++) {
+        ret_mat(i,i) = std::exp(trans_vals(i))*(upper_bounds(i) - lower_bounds(i)) / std::pow(std::exp(trans_vals(i)) + 1,2);
+    }
+    //
+    return ret_mat;
 }
