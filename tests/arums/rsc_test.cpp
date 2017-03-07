e5187e3e9fef0ffce3e993645be0937a50ef5100
Keith O'Hara <kthohr@users.noreply.github.com>
2017-03-06 20:01:54 -0500

    update

--- tests/arums/rsc_test.cpp
+++ tests/arums/rsc_test.cpp
@@ -6,7 +6,7 @@
  *
  * cd ~/Desktop/SCM/GitHub/TraME/src/trame/tests/arums
  *
- * g++-mp-5 -O2 -Wall -std=c++11 -I/opt/local/include -I./../../headers -I/usr/local/include rsc_test.cpp -o rsc.test -L/opt/local/lib -ltrame -framework Accelerate
+ * g++-mp-5 -O2 -Wall -std=c++11 -I/opt/local/include -I/opt/local/include/trame -I/usr/local/include rsc_test.cpp -o rsc.test -L/opt/local/lib -ltrame -framework Accelerate
  */
 
 #include "trame.hpp"
