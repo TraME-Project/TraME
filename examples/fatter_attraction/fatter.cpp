1c4cd41f41d201d6e04a48224b65026ba4202771
Keith O'Hara <kthohr@users.noreply.github.com>
2017-08-28 15:56:10 -0400

    add Fatter Attraction example

---
+++ examples/fatter_attraction/fatter.cpp
@@ -0,0 +1,71 @@
+
+
+/*################################################################################
+  ##
+  ##   Copyright (C) 2015 - 2017 the TraME Team:
+  ##      Alfred Galichon
+  ##      Keith O'Hara
+  ##
+  ##   This file is part of TraME.
+  ##
+  ##   TraME is free software: you can redistribute it and/or modify
+  ##   it under the terms of the GNU General Public License as published by
+  ##   the Free Software Foundation, either version 2 of the License, or
+  ##   (at your option) any later version.
+  ##
+  ##   TraME is distributed in the hope that it will be useful,
+  ##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ##   GNU General Public License for more details.
+  ##
+  ##   You should have received a copy of the GNU General Public License
+  ##   along with TraME. If not, see <http://www.gnu.org/licenses/>.
+  ##
+  ################################################################################*/
+
+// g++-mp-7 -O3 -Wall -std=c++11 -fopenmp -I./../../include fatter.cpp -o fatter.test -framework Accelerate -L./../.. -ltrame
+
+#include "trame.hpp"
+
+int main()
+{
+
+    //
+    // load data
+
+    arma::mat Xvals, Yvals;
+	Xvals.load("data/Xvals.txt",arma::auto_detect);
+    Yvals.load("data/Yvals.txt",arma::auto_detect);
+    
+
+    const int nbX = Xvals.n_rows;
+    const int nbY = Yvals.n_rows;
+
+    arma::mat mu_hat = arma::eye(nbX,nbY);
+
+    arma::vec n = arma::ones(nbX,1);
+    arma::vec m = arma::ones(nbY,1);
+
+    //
+    // estimate
+
+    trame::model< trame::mfe<trame::mmfs::geo> > aff_model;
+
+    aff_model.build(Xvals,Yvals,n,m);
+
+    double lambda = 0.58, val_hat_1, val_hat_2;
+
+    arma::mat theta_hat_aff;
+
+    aff_model.mme_regul(mu_hat,lambda,theta_hat_aff,val_hat_1,nullptr,nullptr,nullptr,nullptr);
+    std::cout << "obj. value with regularization: " << val_hat_1 << std::endl;
+
+    arma::cout << "theta_hat:\n" << theta_hat_aff << arma::endl;
+
+    aff_model.mme_woregul(mu_hat,theta_hat_aff,val_hat_2,nullptr,nullptr,nullptr,nullptr,nullptr);
+    std::cout << "obj. value without regularization: " << val_hat_2 << std::endl;
+    
+    arma::cout << "theta_hat:\n" << theta_hat_aff << arma::endl;
+
+    return 0;
+}
